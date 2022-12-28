0f3098bf26f74cbbab19e95fe1247569360f1ea3
Madjosz <28844868+Madjosz@users.noreply.github.com>
2022-12-28 02:07:03 +0100

    make it move down as simple as possible

--- Moving.java
+++ Moving.java
@@ -3,16 +3,11 @@ import javax.swing.SwingUtilities;
 public class Moving extends Thread{
 	
 	Tetris obj = new Tetris();
-	Pieces o = new Pieces();
-	Tetris.Board ob = obj.new Board();
 
 	public void run() {
 		int a=0;
 		while(a<3) {
-//			ob.moveBlockDown();
-			o.moveDown();
-			obj.repaint();
-			System.out.println(o.getY());
+			obj.board.moveBlockDown();
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
